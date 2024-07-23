44ea3d102d03a25caba6df48ccf2dff1b059896c
MyCloudAtl <megandmashburn@gmail.com>
2024-07-23 12:05:07 -0400

    Setup of Frontend beginning Components

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+    <div>
+        <div className ="Footer">
+            <a className=" footerlink" href ="https://github.com/MyCloudAtl" target="_blank">Megan Mashburn</a>
+            <a className="git footerlink" href ="https://github.com/MyCloudAtl/XploreATL.git" target="_blank">Github Page</a>
+
+        </div>
+    </div>
+    )
+}
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+
+  return (
+    <div className="Home">
+    </div>
+  )
+}
